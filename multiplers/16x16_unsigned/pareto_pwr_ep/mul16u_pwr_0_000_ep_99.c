557886160f579a02b4584a2139e5861e7fa352dc
37452722 <37452722+zvasicek@users.noreply.github.com>
2019-01-08 17:48:49 +0100

    v5

--- multiplers/16x16_unsigned/pareto_pwr_ep/mul16u_pwr_0_000_ep_99.c
+++ multiplers/16x16_unsigned/pareto_pwr_ep/mul16u_pwr_0_000_ep_99.c
@@ -1,21 +1,15 @@
 /***
-* This code is a part of ApproxLib library (ehw.fit.vutbr.cz/approxlib) distributed under a XXXX public license.
-* When used, please cite the following article: tbd 
-* This file is pareto optimal sub-set in the pdk45_pwr and ep% parameters
+* This code is a part of ApproxLib library (ehw.fit.vutbr.cz/approxlib) distributed under The MIT License.
+* When used, please cite the following article(s): V. Mrazek, Z. Vasicek, L. Sekanina, H. Jiang and J. Han, "Scalable Construction of Approximate Multipliers With Formally Guaranteed Worst Case Error" in IEEE Transactions on Very Large Scale Integration (VLSI) Systems, vol. 26, no. 11, pp. 2572-2576, Nov. 2018. doi: 10.1109/TVLSI.2018.2856362 
+* This file contains a circuit from a sub-set of pareto optimal circuits with respect to the pwr and ep parameters
 ***/
 #include <stdint.h>
 #include <stdlib.h>
 
 uint32_t mul16u_pwr_0_000_ep_99(uint16_t A, uint16_t B)
 {
-  uint32_t P, P_;
-  uint16_t tmp, S_15_15,S_16_14;
-  S_15_15 = (((A>>15)&1) & ((B>>15)&1));
-  S_16_14 = S_15_15;
-  P = 0;
-  P |= (S_16_14 & 1) << 30;
-  return P;
+ return ((A >> 15) * (B >> 15)) << (2*15);
 }
 
-// internal reference: truncation-tm.16.trun16_tam15b
+// internal reference: truncation-tm.16.trun16_tm15a
 
--- multiplers/8x8_unsigned/pareto_pwr_ep/mul8u_pwr_0_000_ep_99.c
+++ multiplers/8x8_unsigned/pareto_pwr_ep/mul8u_pwr_0_000_ep_99.c
@@ -1,7 +1,7 @@
 /***
-* This code is a part of ApproxLib library (ehw.fit.vutbr.cz/approxlib) distributed under a XXXX public license.
-* When used, please cite the following article: tbd 
-* This file is pareto optimal sub-set in the pdk45_pwr and ep% parameters
+* This code is a part of ApproxLib library (ehw.fit.vutbr.cz/approxlib) distributed under The MIT License.
+* When used, please cite the following article(s): V. Mrazek, R. Hrbacek, Z. Vasicek and L. Sekanina, "EvoApprox8b: Library of approximate adders and multipliers for circuit design and benchmarking of approximation methods". Design, Automation & Test in Europe Conference & Exhibition (DATE), 2017, Lausanne, 2017, pp. 258-261. doi: 10.23919/DATE.2017.7926993 
+* This file contains a circuit from a sub-set of pareto optimal circuits with respect to the pwr and ep parameters
 ***/
 #include <stdint.h>
 #include <stdlib.h>
@@ -12,24 +12,24 @@ uint64_t mul8u_pwr_0_000_ep_99(const uint64_t B,const uint64_t A)
 
 
    O = 0;
-   O |= (((B >> 2)&1)&1) << 0;
-   O |= (((A >> 2)&1)&1) << 1;
-   O |= (((B >> 7)&1)&1) << 2;
+   O |= (0&1) << 0;
+   O |= (0&1) << 1;
+   O |= (0&1) << 2;
    O |= (0&1) << 3;
    O |= (0&1) << 4;
-   O |= (((A >> 2)&1)&1) << 5;
+   O |= (0&1) << 5;
    O |= (0&1) << 6;
    O |= (0&1) << 7;
-   O |= (((B >> 0)&1)&1) << 8;
-   O |= (((A >> 0)&1)&1) << 9;
+   O |= (0&1) << 8;
+   O |= (0&1) << 9;
    O |= (0&1) << 10;
-   O |= (((A >> 6)&1)&1) << 11;
-   O |= (((A >> 7)&1)&1) << 12;
-   O |= (((A >> 7)&1)&1) << 13;
+   O |= (0&1) << 11;
+   O |= (0&1) << 12;
+   O |= (0&1) << 13;
    O |= (0&1) << 14;
-   O |= (((B >> 7)&1)&1) << 15;
+   O |= (0&1) << 15;
    return O;
 }
 
-// internal reference: cgp-approx14.08.mult8_cgp14_wc33411_wtmrca
+// internal reference: cgp-approx14zr.08.mult8_cgp14zr_wc65025_rcam
 
